9340fc4732e6bc89ed5f146e6a7ddded8cab69fb
duxin <duxin@jiuan.com>
2014-08-24 23:27:02 +0800

    用代理写了一版实现~

--- firstGoal/BCChildController.h
+++ firstGoal/BCChildController.h
@@ -1,15 +1,18 @@
 //
-//  BCViewController.h
+//  BCChildController.h
 //  firstGoal
 //
-//  Created by BifidyCAPs on 14/8/19.
+//  Created by ios90 on 14-8-24.
 //  Copyright (c) 2014年 BifidyCAPs. All rights reserved.
 //
-
+@protocol viewDelegate <NSObject>
+-(void)refresh;
+@end
 #import <UIKit/UIKit.h>
-#import "BCChildController.h"
-@interface BCViewController : UIViewController<viewDelegate>
+
+@interface BCChildController : UIViewController
 @property (nonatomic,weak)IBOutlet UILabel *label;
 @property (nonatomic,weak)IBOutlet UITextField *field;
+@property (nonatomic,weak)id<viewDelegate> parentView;
 -(IBAction)click:(id)sender;
 @end
--- firstGoal/BCViewController.m
+++ firstGoal/BCViewController.m
@@ -9,21 +9,52 @@
 #import "BCViewController.h"
 
 @interface BCViewController ()
-
+@property(nonatomic,strong)BCChildController *childVC;
 @end
 
 @implementation BCViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad{
     [super viewDidLoad];
-    NSLog(@"This is first goal");
+    //添加键盘收回
+    [self.field addTarget:_field action:@selector(resignFirstResponder) forControlEvents:UIControlEventEditingDidEndOnExit];
+    [self.childVC.field addTarget:self.childVC.field action:@selector(resignFirstResponder) forControlEvents:UIControlEventEditingDidEndOnExit];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+//点击赋值
+-(IBAction)click:(id)sender{
+    //completion是block参数，在跳转成功后的执行代码
+    [self presentViewController:self.childVC animated:YES completion:^{
+    self.childVC.label.text = self.field.text;
+    }];
 }
+
+//作为childVC的代理方法实现
+-(void)refresh{
+    [self.childVC dismissViewControllerAnimated:YES completion:^{
+    self.label.text = self.childVC.field.text;
+    }];
+}
+
+//延迟实例化，两个效果不同，自己参考~
+-(BCChildController *)childVC{
+    if(_childVC==nil) {
+        _childVC = [self.storyboard instantiateViewControllerWithIdentifier:@"childVC"];
+        _childVC.parentView = self;
+    }
+    return _childVC;
+}
+/*
+//静态单例
+-(BCChildController *)childVC{
+    static BCChildController *VC = nil;
+    if (!VC) {
+        VC = [self.storyboard instantiateViewControllerWithIdentifier:@"childVC"];
+        VC.parentView = self;
+    }
+    return VC;
+ 
+}
+*/
 
 @end
